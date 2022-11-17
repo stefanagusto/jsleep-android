ef4757788dab1177df6cabb4780d1ca3b59e6700
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2022-11-18 02:15:47 +0700

    Post Test 8

--- app/src/main/java/com/stefanagustohutapeajsleepdn/jsleep_android/AboutMeActivity.java
+++ app/src/main/java/com/stefanagustohutapeajsleepdn/jsleep_android/AboutMeActivity.java
@@ -1,13 +1,8 @@
 package com.stefanagustohutapeajsleepdn.jsleep_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutMeActivity extends AppCompatActivity {
 
