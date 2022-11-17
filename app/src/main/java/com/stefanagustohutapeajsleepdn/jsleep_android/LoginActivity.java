68ae20f90ed7960f2a18a59215b68ba131230f00
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2022-11-17 23:08:54 +0700

    Post Test 8 (Sementara)

--- app/src/main/java/com/stefanagustohutapeajsleepdn/jsleep_android/LoginActivity.java
+++ app/src/main/java/com/stefanagustohutapeajsleepdn/jsleep_android/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -32,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent move = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(move);
+                Toast.makeText(getApplicationContext(),"Login Successful", Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/stefanagustohutapeajsleepdn/jsleep_android/MainActivity.java
+++ app/src/main/java/com/stefanagustohutapeajsleepdn/jsleep_android/MainActivity.java
@@ -3,6 +3,8 @@ package com.stefanagustohutapeajsleepdn.jsleep_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        getMenuInflater().inflate(R.menu.menu_resource, menu);
+        return true;
     }
 }
