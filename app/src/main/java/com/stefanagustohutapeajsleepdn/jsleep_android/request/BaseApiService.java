48578a0b71f0613fb84232d272cfd4b0abd37288
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2022-11-22 12:53:44 +0700

    Update Jsleepandroid terbaru

--- app/src/main/java/com/stefanagustohutapeajsleepdn/jsleep_android/request/BaseApiService.java
+++ app/src/main/java/com/stefanagustohutapeajsleepdn/jsleep_android/request/BaseApiService.java
@@ -1,6 +1,8 @@
 package com.stefanagustohutapeajsleepdn.jsleep_android.request;
 
 import com.stefanagustohutapeajsleepdn.jsleep_android.model.Account;
+import com.stefanagustohutapeajsleepdn.jsleep_android.model.Room;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -9,4 +11,8 @@ public interface BaseApiService {
 
     @GET("account/{id}")
     Call<Account> getAccount(@Path("id") int id);
+    @GET("room/{id}")
+    Call<Room> getRoom(@Path("id") int id);
+    @GET("renter/{id}")
+    Call<Room> getRenter(@Path("id") int id);
 }
